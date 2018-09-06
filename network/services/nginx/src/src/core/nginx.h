386aa83b82e74162868ea7dc92ca03bbff576fce
zhanyige <zhanyige@veryci.com>
2018-09-06 02:50:57 -0700

    delete  404 nginx ver

--- network/services/nginx/src/src/core/nginx.h
+++ network/services/nginx/src/src/core/nginx.h
@@ -10,8 +10,8 @@
 
 
 #define nginx_version      1012000
-#define NGINX_VERSION      "1.12.0"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define NGINX_VERSION      " "
+#define NGINX_VER          " " NGINX_VERSION
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
