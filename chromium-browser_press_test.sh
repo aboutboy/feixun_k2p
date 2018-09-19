81aca97406ef12a226c114bb2835c7d0510c543c
zhanyige <zhanyige@veryci.com>
2018-09-19 06:42:15 -0700

    modify chrome press shell

--- chromium-browser_press_test.sh
+++ chromium-browser_press_test.sh
@@ -2,15 +2,40 @@
 rm -rf test.log
 while :
 do
-	chromium-browser http://finance.sina.com.cn/zl/china/2018-08-24/zl-ihicsiaw4320984.shtml &
-	sleep 24; killall chromium-browser
+	chromium-browser http://finance.sina.com.cn/zl/china/2018-08-12/zl-ihicsiaw4320984.shtml &
+	sleep 12; killall chromium-browser
 	chromium-browser http://zhuanlan.sina.com.cn/ &
-	sleep 24; killall chromium-browser
+	sleep 12; killall chromium-browser
 	chromium-browser  http://sports.sina.com.cn/zl/ &
-	sleep 24; killall chromium-browser
+	sleep 12; killall chromium-browser
 	chromium-browser http://sports.sina.com.cn/zl/basketball/2018-08-25/zldoc-ihicsiaw8441084.shtml &
-	sleep 24; killall chromium-browser
+	sleep 12; killall chromium-browser
 	chromium-browser http://sports.sina.com.cn/zl/basketball/2018-08-22/zldoc-ihhzsnea7876731.shtml &
-	sleep 24; killall chromium-browser
+	sleep 12; killall chromium-browser
+	chromium-browser http://edu.163.com/liuxue/#f=endnav &
+	sleep 12; killall chromium-browser
+	chromium-browser http://tech.163.com/smart/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://tech.163.com/special/kaiwu/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://tech.163.com/special/techat0007/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://tech.163.com/special/kaiwu/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://edu.163.com/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://v.163.com/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://live.163.com/room/174866.html &
+	sleep 12; killall chromium-browser
+	chromium-browser http://live.163.com/room/178298.html &
+	sleep 12; killall chromium-browser
+	chromium-browser http://foxue.163.com/ &
+	sleep 12; killall chromium-browser
+	chromium-browser http://jiankang.163.com/ &
+	sleep 12; killall chromium-browser
+
+
+
 	echo $(date) >> test.log 
 done
